7c0abcbb15c2b9a7b86c26f2b3698f648f916c0a
“pratik” <pratikbhandurge2222@gmail.com>
2022-08-16 16:12:30 +0530

    Uc1

--- MaxNumberUsingGenerics/Program.cs
+++ MaxNumberUsingGenerics/Program.cs
@@ -6,7 +6,15 @@ namespace MaxNumberUsingGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Max Number Program!");
+            Console.WriteLine("Find Maximum Number");
+            int num1 = 20;
+            int num2 = 25;
+            int num3 = 30;
+            GenericMaximum<int> maximum = new GenericMaximum<int>(num1, num2, num3);
+            int max = maximum.MaxMethod();
+            Console.WriteLine(max);
+
         }
     }
 }
